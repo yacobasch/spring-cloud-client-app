9814004f6ac915b593f42c9b06e9b389ce87a3ba
eMRtY08XXYDK <yacobaschalew@gmail.com>
2021-02-02 11:29:55 -0600

    Client App configured to access the Config files through Config Server

---
+++ src/main/java/com/nyala/webapp/SpringcloudClientApplication.java
@@ -0,0 +1,13 @@
+package com.nyala.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nyala/webapp/SpringcloudClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nyala.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
