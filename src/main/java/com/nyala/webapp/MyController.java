a72a50300682ca1cdd43297077ce7e5483e0050c
eMRtY08XXYDK <yacobaschalew@gmail.com>
2021-02-02 13:08:31 -0600

    application yaml config for client

--- src/main/java/com/nyala/webapp/MyController.java
+++ src/main/java/com/nyala/webapp/MyController.java
@@ -9,9 +9,17 @@ public class MyController {
 
 	@Value("${lucky-word}")
 	String luckyWord;
+	
+	//@Value("${env}")
+	String environmnent;
 
 	@GetMapping("/lucky-word")
 	public String showLuckyWord() {
 		return "The lucky word is: " + luckyWord;
+	}
+	
+	@GetMapping("/env")
+	public String showEnv() {
+		return "The Env is: " + environmnent;
 	}
 }
